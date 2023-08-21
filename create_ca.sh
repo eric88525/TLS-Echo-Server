700d2192df6b196aed8bc6f8e0cff8afed554b80
eric88525 <eric885251@gmail.com>
2023-08-21 21:32:02 +0800

    refactor: ca folder

--- create_ca.sh
+++ create_ca.sh
@@ -1,24 +1,24 @@
 #!bin/bash
 
+mkdir certificates
+cd certificates
+
 # Create a CA certificate
-mkdir ca
-openssl genpkey -algorithm RSA -out ca/ca-private.key
+openssl genpkey -algorithm RSA -out ca-private.key
 openssl req -new -key ca-private.key -x509 -days 365 -out ca-cert.pem
 
 
 # Create a server certificate
-mkdir server
-openssl genpkey -algorithm RSA -out server/server-private.key
-openssl req -new -key server/server-private.key -out server/server.csr
+openssl genpkey -algorithm RSA -out server-private.key
+openssl req -new -key server-private.key -out server.csr
 
 # Sign the server certificate with the CA
-openssl x509 -req -in server/server.csr -CA ca/ca-cert.pem -CAkey ca/ca-private.key -CAcreateserial -out server/server-cert.pem -days 365
+openssl x509 -req -in server.csr -CA ca-cert.pem -CAkey ca-private.key -CAcreateserial -out server-cert.pem -days 365
 
 
 # Create a client certificate
-mkdir client
-openssl genpkey -algorithm RSA -out client/client-private.key
-openssl req -new -key client/client-private.key -out client/client.csr
+openssl genpkey -algorithm RSA -out client-private.key
+openssl req -new -key client-private.key -out client.csr
 
 # Sign the client certificate with the CA
-openssl x509 -req -in client/client.csr -CA ca/ca-cert.pem -CAkey ca/ca-private.key -CAcreateserial -out client/client-cert.pem -days 365
+openssl x509 -req -in client.csr -CA ca-cert.pem -CAkey ca-private.key -CAcreateserial -out client-cert.pem -days 365
