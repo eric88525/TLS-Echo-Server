cbbe7539f03dd1301ef51025f028ec3fce390492
eric88525 <eric885251@gmail.com>
2023-08-21 20:05:45 +0800

    refactor: make client more save

--- myClient.c
+++ myClient.c
@@ -5,108 +5,118 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-#include <sys/wait.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 #define BUFSIZE 1024
-#define CA_FILE             "./CA/cacert.pem"
-#define CLIENT_KEY          "./client/key.pem"
-#define CLIENT_CERT         "./client/cert.pem"
 
-void setCA(SSL_CTX *ctx){
-    // SSL_VERIFY_PEER = verify both  
+#define CA_FILE       "ca/ca-cert.pem"
+#define CLIENT_KEY    "client/client-private.key"
+#define CLIENT_CERT   "client/client-cert.pem"
+
+void setCA(SSL_CTX *ctx) {
     SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, 0);
+
     if (SSL_CTX_load_verify_locations(ctx, CA_FILE, 0) != 1) {
-        SSL_CTX_free(ctx);
-        printf("Failed to load CA file %s", CA_FILE);
+        perror("Failed to load CA file");
+        exit(1);
     }
-    //load server cert
+
     if (SSL_CTX_use_certificate_file(ctx, CLIENT_CERT, SSL_FILETYPE_PEM) <= 0) {
         ERR_print_errors_fp(stdout);
         exit(1);
-    }else{
-        puts("load CLIENT_CERT success");
+    } else {
+        puts("Loaded CLIENT_CERT successfully");
     }
-    // load server private key
+
     if (SSL_CTX_use_PrivateKey_file(ctx, CLIENT_KEY, SSL_FILETYPE_PEM) <= 0) {
-        printf("load private key fail.\n");
+        perror("Failed to load private key");
         ERR_print_errors_fp(stdout);
         exit(1);
-    }else{
-        puts("load CLIENT_KEY success");
+    } else {
+        puts("Loaded CLIENT_KEY successfully");
     }
-    // check if cert and private key is ok
+
     if (!SSL_CTX_check_private_key(ctx)) {
+        perror("Certificate and private key do not match");
         ERR_print_errors_fp(stdout);
         exit(1);
-    }else{
-        puts("cert and privateKey ok!");
+    } else {
+        puts("Certificate and private key match!");
     }
 }
-void showCert(SSL * ssl){
+
+void showCert(SSL *ssl) {
     X509 *cert;
     char *line;
     cert = SSL_get_peer_certificate(ssl);
     if (cert != NULL) {
         printf("Server certificate:\n");
         line = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0);
-        printf("1.subject_name: %s\n", line);
-        free(line);
+        printf("1. subject_name: %s\n", line);
+        OPENSSL_free(line);
         line = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0);
-        printf("2.issuer_name%s\n", line);
-        free(line);
+        printf("2. issuer_name: %s\n", line);
+        OPENSSL_free(line);
         X509_free(cert);
     } else {
-        printf("No Cert exitst！\n");
+        printf("No Cert exists!\n");
     }
-    puts("Cipher type");
-    puts(SSL_get_cipher(ssl));
+    printf("Cipher type: %s\n", SSL_get_cipher(ssl));
 }
-int main(int argc , char *argv[])
-{
 
-    //----------------------------------------------- ssl region
+int main(int argc, char *argv[]) {
     SSL_library_init();
-    SSL_CTX *ctx = SSL_CTX_new (TLSv1_2_client_method()); 
+    SSL_CTX *ctx = SSL_CTX_new(TLS_client_method());
     setCA(ctx);
-    SSL * ssl = SSL_new(ctx);
-    //---------------------- --------------------------socket region
 
-    //socket的建立
+    SSL *ssl = SSL_new(ctx);
+
     int sockfd = 0;
-    sockfd = socket(AF_INET , SOCK_STREAM , 0);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (sockfd == -1){
-        printf("Fail to create a socket.");
+    if (sockfd == -1) {
+        perror("Fail to create a socket.");
+        exit(1);
     }
-    //socket的連線
+
     struct sockaddr_in info;
-    bzero(&info,sizeof(info));
+    bzero(&info, sizeof(info));
     info.sin_family = PF_INET;
-    //localhost test
     info.sin_addr.s_addr = inet_addr("127.0.0.1");
     info.sin_port = htons(atoi(argv[1]));
-    int err = connect(sockfd,(struct sockaddr *)&info,sizeof(info));
-    if(err==-1){
-        printf("Connection error");
+
+    int err = connect(sockfd, (struct sockaddr *)&info, sizeof(info));
+    if (err == -1) {
+        perror("Connection error");
+        exit(1);
     }
-    //---------------------------------------------------------------
-    SSL_set_fd(ssl,sockfd);
-    if(SSL_connect(ssl)==-1){
-        puts("SSL connect fail!");
-    }else{
+
+    SSL_set_fd(ssl, sockfd);
+    if (SSL_connect(ssl) != 1) {
+        perror("SSL connect fail!");
+        ERR_print_errors_fp(stdout);
+        exit(1);
+    } else {
         puts("SSL connect success!");
-        
     }
+
     showCert(ssl);
-    //----------------------------------------------------------------
+
+    //char message[BUFSIZE + 1] = "Hello, server!";
+
     char message[BUFSIZE+1]={};
-    gets(message);
-    SSL_write(ssl,message,strlen(message));
-    SSL_read (ssl, message, sizeof(message));
-    puts(message);
+
+    scanf("%s", message);
+    //gets(message);
+
+    SSL_write(ssl, message, strlen(message));
+
+    memset(message, 0, sizeof(message));
+    SSL_read(ssl, message, sizeof(message));
+    printf("Received message from server: %s\n", message);
+
     SSL_shutdown(ssl);
     SSL_free(ssl);
     close(sockfd);
